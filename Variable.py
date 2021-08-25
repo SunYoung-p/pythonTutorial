4d9181c9dd660a3951099bb8f6b490456a304c82
PSY <p2sy45@gmail.com>
2021-08-26 02:39:08 +0900

    Variable

---
+++ Variable.py
@@ -0,0 +1,11 @@
+
+def varTutorial():
+    x = 2 * 2 * 2
+    x = x / 4
+    x = x * x
+    print(x)
+
+    x, y = 10, 20
+    print("x is ", x, "/ y is ", y)
+    x, y = y, x # swap
+    print("x is ", x, "/ y is ", y)
